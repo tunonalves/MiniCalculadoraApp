75ff836bb1de06d1e5433ea1e9ccb5616e430554
Federico Tuñon Alves <70298964+tunonalves@users.noreply.github.com>
2021-10-28 23:08:43 -0300

    calculos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,11 +11,22 @@ export class AppComponent {
   numero2: number = 0;
   resultado: number = 0;
 
-  Suma(event:Event):void{
+  Suma():void{
     this.resultado = this.numero1 + this.numero2;
   } 
 
-  Resta(event:Event):void{
+  Resta():void{
     this.resultado = this.numero1 - this.numero2;
+  } 
+
+  Multi():void{
+    this.resultado = this.numero1 * this.numero2;
+  } 
+  
+  Divi():void{
+    if(this.numero1 != 0)
+      this.resultado = this.numero1 / this.numero2;
+    else
+      alert("Error, divicion por 0")
   } 
 }
